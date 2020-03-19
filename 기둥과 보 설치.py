1b22ce1d2c7589af607620c67d185057fe1a447a
jsrimr <jsrimr@naver.com>
2020-03-19 16:39:14 +0900

    수정버전

--- 기둥과 보 설치.py
+++ 기둥과 보 설치.py
@@ -5,11 +5,11 @@ def solution(n, build_frame):
         if y == n: #바닥이면
             return True
 
-        if map_[y+1][x][0]:# 밑에 기둥 있으면
+        if (y+1) <= n and map_[y+1][x][0]:# 밑에 기둥 있으면
             return True 
         if map_[y][x][1]: #보가 있으면
             return True
-        if map_[y][x-1][1]: #하나 옆에 보가 있으면
+        if (x-1) >=0 and map_[y][x-1][1]: #하나 옆에 보가 있으면
             return True
 
         return False
@@ -21,7 +21,7 @@ def solution(n, build_frame):
         if y == n or x == n: #맨 오른쪽이나 바닥에는 보 설치 불가
             return False
 
-        if map_[y+1][x][0] or map_[y+1][x+1][0]: #왼쪽이나 오른쪽에 기둥이 있다면 설치가능
+        if ((y+1) <= n and map_[y+1][x][0]) or ((x+1) <= n and map_[y+1][x+1][0]): #왼쪽이나 오른쪽에 기둥이 있다면 설치가능
             return True
 
         if x>0 and x<n and map_[y][x-1][1] and map_[y][x+1][1]: #같은 높이에 양쪽으로 보가 있다면 설치가능
@@ -33,16 +33,16 @@ def solution(n, build_frame):
     def pillar_destroy_possible(map_, pillar_x_y):
         x, y = pillar_x_y
         
-        # 위에 쌓인 기둥 있으면 아웃  
-        if map_[y-1][x][0]:
-            return False
-
-        # 보가 있다면 서로 이어져 있지 않으면 아웃
-        if map_[y-1][x][1]:
-
-            if x == 0:  # 인덱스 오류 예외
+        # 위에 쌓인 기둥 있으면 아웃 => 이 기둥이 다른 보에 의해 받쳐지고 있을 수도 있으니 함부로 아웃 시키면 안됨. 이런 접근이 좋지가 않다. 일단 저지르고 괜찮은지 파악하는 check 방식이 좋은 이유
+        if (y-1)>0 and map_[y-1][x][0]:
+            # 기둥이 보에 의해 받쳐지지 않으면 아웃 
+            if (not map_[y-1][x][0]) and (x-1)>0 and (not map_[y-1][x-1][0]):
                 return False
-            if not map_[y-1][x-1][1]:
+
+        # 기둥 위에 보가 있다면 서로 이어져 있지 않으면 아웃
+        if (y-1)>0 and map_[y-1][x][1]:
+
+            if ((x-1)>0 and not map_[y-1][x-1][1]) or ((x+1)<=n and not map_[y-1][x+1][1]) :
                 return False
 
         return True
@@ -53,26 +53,25 @@ def solution(n, build_frame):
 
         #다음 중 하나라도 해당되는 부분이 있다면 out
 
-        # 보가 떠받치던 기둥이 있었다면,
+        # 보 왼쪽에 떠받치던 기둥이 있었다면,
         if map_[y][x][0]:
-            # 옆의 다른 보가 기둥을 지탱해주지 않으면 아웃
-            if (not map_[y][x-1][1]):
+            # 옆의 다른 보가 기둥을 지탱해주지 않으면 아웃 => 밑의 기둥이 지탱해줄수도 있다
+            if ((x-1)<0 or not map_[y][x-1][1]) and ((y+1)<=n or not map_[y+1][x][0]):
                 return False
 
-        # 오른쪽에 보가 떠받치던 기둥이 있었다면,
-        if map_[y][x+1][0]:
-            #
-            if (x+1 > n) or (not map_[y][x+1][1]):
+        # 보 오른쪽에 떠받치던 기둥이 있었는데, 다른 보나 기둥에 의해 받쳐지지 않는다면 아웃
+        if (x+1) <=n and map_[y][x+1][0]:
+            if  (not map_[y][x+1][1]) and ((y+1)>n or not map_[y+1][x+1][0]):
                 return False
 
         # 이어진 보(con)가 있다면, con이 다른 기둥위에 있지 않으면 아웃
-        if (x+1)<n and map_[y][x+1][1]:
-            if not map_[y+1][x+1][0]:
+        if (x+1)<=n and map_[y][x+1][1]:
+            if (y+1) > n or (not map_[y+1][x+1][0]):
                 return False
 
         # 이어진 보(con)가 왼쪽에 있다면, con이 다른 기둥위에 있지 않으면 아웃
         if x>0 and map_[y][x-1][1]:
-            if not map_[y+1][x-1][0]:
+            if (y+1) > n or (not map_[y+1][x-1][0]):
                 return False
 
         return True
