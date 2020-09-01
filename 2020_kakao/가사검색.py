c7b27369c08e2c4774d33c54291c78307c91dab4
jsrimr <jsrimr@naver.com>
2020-09-01 22:56:15 +0900

    10분 더해서 생각한 로직은 다 짰음. 그런데 답이 틀림.

--- 2020_kakao/가사검색.py
+++ 2020_kakao/가사검색.py
@@ -3,9 +3,9 @@ class Node(object):
     A node that consists of a trie.
     """
 
-    def __init__(self, key):
+    def __init__(self, key, data=None):
         self.key = key
-        self.data = False
+        self.data = data
         self.children = {}
 
 
@@ -13,17 +13,41 @@ class Trie(object):
     def __init__(self):
         self.head = Node(None)
 
+    """
+    트라이에 문자열을 삽입합니다.
+    """
+
     def insert(self, string):
         curr_node = self.head
 
         for char in string:
-            curr_node.data += 1
-
             if char not in curr_node.children:
                 curr_node.children[char] = Node(char)
+
             curr_node = curr_node.children[char]
 
-        curr_node.data = True
+            # string 의 마지막 글자 차례이면,
+            # 노드의 data 필드에 저장하려는 문자열 전체를 저장한다.
+        curr_node.data = string
+
+    """
+    주어진 단어 string이 트라이에 존재하는지 여부를 반환합니다.
+    """
+
+    def search(self, string):
+        curr_node = self.head
+
+        for char in string:
+            if char in curr_node.children:
+                curr_node = curr_node.children[char]
+            else:
+                return False
+
+        # string의 마지막 글자에 다달았을 때,
+        # curr_node 에 data 가 있다면 string이 트라이에 존재하는 것!
+        if (curr_node.data != None):
+            return True
+
     """
     주어진 prefix 로 시작하는 단어들을
     트라이에서 찾아 리스트 형태로 반환합니다.
@@ -31,25 +55,36 @@ class Trie(object):
 
     def starts_with(self, prefix):
         curr_node = self.head
-        result = 0
-        # 트라이에서 prefix 를 찾고,
-        for char in prefix: #
+        subtrie = None
 
+        # 트라이에서 prefix 를 찾고,
+        # prefix의 마지막 글자 노드를 subtrie로 설정
+        for char in prefix:
+            if char == "?":
+                break
             if char in curr_node.children:
                 curr_node = curr_node.children[char]
+                subtrie = curr_node
             else:
-                return 0
+                return None
+
         # bfs 로 prefix subtrie를 순회하며
         # data가 있는 노드들(=완전한 단어)를 찾는다.
-        queue = list(curr_node.children.values())
+        queue = list(zip(subtrie.children.values(), [0] * len(subtrie.children)))  # Node list
 
+        count_question_m = prefix.count("?")
+        result = 0
         while queue:
-            curr = queue.pop()
-            if curr.data:
+            curr, depth = queue.pop()
+            if depth > count_question_m:
+                break
+            if depth == count_question_m and curr.data != None:
+                # result.append(curr.data)
                 result += 1
-            queue += list(curr.children.values())
+            queue += list(zip(curr.children.values(), [depth + 1] * len(curr.children)))
 
         return result
+
 
 def solution(words, queries):
     ordered_Trie = Trie()
