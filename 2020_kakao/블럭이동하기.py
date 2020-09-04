b6412dae7af9a9efa12d510709849d2cac1a6efa
jsrimr <jsrimr@naver.com>
2020-09-04 21:41:33 +0900

    초기상태 잘못 설정한거 수정. 0,0,0,0 -> 0,0,0,1

--- 2020_kakao/블럭이동하기.py
+++ 2020_kakao/블럭이동하기.py
@@ -6,72 +6,79 @@ class Node:
         self.r1, self.r2, self.c1, self.c2 = r1, r2, c1, c2
         self.state = state
         self.cnt = cnt
+
     def getXY(self):
-        if self.state == "long":
-            return min(self.r1, self.r2), self.c1
-        else:
-            return self.r1, min(self.c1, self.c2)
+        return self.r1, self.c1
+        # if self.state == "long":
+        #     # return min(self.r1, self.r2), self.c1
+        #     return self.r1, self.c1
+        # else:
+        #     return self.r1, self.c1  # min(self.c1, self.c2)
 
     def getXY2(self):
-        if self.state == "long":
-            return max(self.r1, self.r2), self.c1
-        else:
-            return self.r1, max(self.c1, self.c2)
+        return self.r2, self.c2
+        # if self.state == "long":
+        #     return self.r2,self.c2#max(self.r1, self.r2), self.c1
+        # else:
+        #     return self.r1, max(self.c1, self.c2)
 
 
 def solution(board):
-    start_node = Node(0, 0, 0, 0, "cross",0)
+    start_node = Node(0, 0, 0, 1, "cross", 0)
     queue = deque([start_node])
     visited = set()
 
     n = len(board)
     while queue:
         node = queue.popleft()
-        if (node.getXY(),node.getXY2()) in visited:
+        if (node.getXY(), node.getXY2()) in visited:
             continue
-        visited.add((node.getXY(),node.getXY2()))
+        visited.add((node.getXY(), node.getXY2()))
 
         r1, c1 = node.getXY()
         r2, c2 = node.getXY2()
         cnt = node.cnt
-        # print(r1,c1,r2,c2,node.state)
+        print(r1, c1, r2, c2, node.state, cnt)
         if (r1, c1) == (n - 1, n - 1) or (r2, c2) == (n - 1, n - 1):
             break
         # 8가지 *2
         if node.state == "cross":  # 가로
             if c2 + 1 <= n - 1 and board[r2][c2 + 1] == 0:  # right empty -> go to right,
-                queue.append(Node(r1, c1 + 1, r2, c2 + 1, "cross",cnt+1))
+                queue.append(Node(r1, c1 + 1, r2, c2 + 1, "cross", cnt + 1))
             if c1 - 1 >= 0 and board[r1][c1 - 1] == 0:  # left empty -> go to left
-                queue.append(Node(r1, c1 - 1, r2, c2 - 1, "cross",cnt+1))
+                queue.append(Node(r1, c1 - 1, r2, c2 - 1, "cross", cnt + 1))
             if r1 - 1 >= 0 and board[r1 - 1][c1] == 0 and board[r2 - 1][c2] == 0:  # up empty -> go up, rotate*2
-                queue.append(Node(r1 - 1, c1, r2 - 1, c2, "cross",cnt+1))
-                queue.append(Node(r1 - 1, c1 + 1, r2, c2, "long",cnt+1))
-                queue.append(Node(r1, c1, r2 - 1, c2 - 1, "long",cnt+1))
-            if r1 + 1 <= n - 1 and board[r1 + 1][c1] == 0 and board[r2 + 1][c2] == 0:  # under empty -> go under, rotate*2
-                queue.append(Node(r1 + 1, c1, r2 + 1, c2, "cross",cnt+1))
-                queue.append(Node(r1 + 1, c1 + 1, r2, c2, "long",cnt+1))
-                queue.append(Node(r1, c1, r2 + 1, c2 - 1, "long",cnt+1))
+                queue.append(Node(r1 - 1, c1, r2 - 1, c2, "cross", cnt + 1))
+                queue.append(Node(r2 - 1, c2, r2, c2, "long", cnt + 1))
+                queue.append(Node(r2 - 1, c1, r2, c1, "long", cnt + 1))
+            if r2 + 1 <= n - 1 and board[r1 + 1][c1] == 0 and board[r2 + 1][
+                c2] == 0:  # under empty -> go under, rotate*2
+                queue.append(Node(r1 + 1, c1, r2 + 1, c2, "cross", cnt + 1))
+                queue.append(Node(r2, c2, r2 + 1, c2, "long", cnt + 1))
+                queue.append(Node(r2, c1, r2 + 1, c1, "long", cnt + 1))
         else:  # long
 
-            if c1 + 1 <= n - 1 and board[r1][c1 + 1] == 0 and board[r2][c2 + 1] == 0:  # 오른쪽이 비었을 때 -> 오른쪽으로 이동, 회전 *2
-                queue.append(Node(r1, c1 + 1, r2, c2 + 1, "long",cnt+1))
-                queue.append(Node(r1 + 1, c1 + 1, r2, c2, "cross",cnt+1))
-                queue.append(Node(r1, c1, r2-1, c1 + 1, "cross",cnt+1))
+            if c2 + 1 <= n - 1 and board[r1][c1 + 1] == 0 and board[r2][c2 + 1] == 0:  # 오른쪽이 비었을 때 -> 오른쪽으로 이동, 회전 *2
+                queue.append(Node(r1, c1 + 1, r2, c2 + 1, "long", cnt + 1))
+                queue.append(Node(r2, c2, r2, c2 + 1, "cross", cnt + 1))
+                queue.append(Node(r1, c1, r1, c1 + 1, "cross", cnt + 1))
             # 왼쪽이 비었을때 -> 왼쪽으로 이동, 회전 *2
-            if c1 - 1 >= 0 and board[r1][c1 - 1] == 0 and board[r1][c2 - 1] == 0:
-                queue.append(Node(r1, c1 - 1, r2, c2 - 1, "long",cnt+1))
-                queue.append(Node(r1 + 1, c1 - 1, r2, c2, "cross",cnt+1))
-                queue.append(Node(r1, c1, r2 - 1, c2 - 1, "cross",cnt+1))
+            if c2 - 1 >= 0 and board[r1][c1 - 1] == 0 and board[r1][c2 - 1] == 0:
+                queue.append(Node(r1, c1 - 1, r2, c2 - 1, "long", cnt + 1))
+                queue.append(Node(r1, c2 - 1, r1, c2, "cross", cnt + 1))
+                queue.append(Node(r2, c2 - 1, r2, c2, "cross", cnt + 1))
             # 위쪽이 비었을로 때 -> 위로이동
             if r1 - 1 >= 0 and board[r1 - 1][c1] == 0:
-                queue.append(Node(r1 - 1, c1, r2 - 1, c2, "long",cnt+1))
+                queue.append(Node(r1 - 1, c1, r2 - 1, c2, "long", cnt + 1))
             # 아래쪽이 비었을 때 -> 아래로 이
             if r2 + 1 <= n - 1 and board[r2 + 1][c1] == 0:
-                queue.append(Node(r1 + 1, c1, r2 + 1, c2, "long",cnt+1))
+                queue.append(Node(r1 + 1, c1, r2 + 1, c2, "long", cnt + 1))
 
     return node.cnt
 
 
 if __name__ == '__main__':
     board = [[0, 0, 0, 1, 1], [0, 0, 0, 1, 0], [0, 1, 0, 1, 1], [1, 1, 0, 0, 1], [0, 0, 0, 0, 0]]
+    board = [[0, 0, 0, 0, 0, 0, 1], [1, 1, 1, 1, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 1, 1, 1, 0],
+             [0, 1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 1, 1], [0, 0, 1, 0, 0, 0, 0]]
     print(solution(board))
