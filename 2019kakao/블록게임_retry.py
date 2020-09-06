d753e7df571e1750e814d626958aff612e0e8cb0
jsrimr <jsrimr@naver.com>
2020-09-06 23:28:16 +0900

    답 찾음! 이미 실패했다고 판단한게 번복될 수 있음. 같은 row 안에서 겹치는 경우가 있을 수 있기 때문. like 6 7 6 7 7 7 6 6

--- 2019kakao/블록게임_retry.py
+++ 2019kakao/블록게임_retry.py
@@ -9,8 +9,8 @@ def solution(board):
         return True
 
     def isA(r, c):
-        if r + 1 <= n - 1 and c + 2 <= n - 1 and board[r][c] == board[r + 1][c] == board[r + 1][c + 1] == \
-                board[r + 1][c + 2]:
+        if r + 1 <= n - 1 and c + 2 <= n - 1 and \
+                board[r][c] == board[r + 1][c] == board[r + 1][c + 1] == board[r + 1][c + 2]:
             if column_clean_until_row(r, c) and column_clean_until_row(r + 1, c + 1) and column_clean_until_row(r + 1,
                                                                                                                 c + 2):
                 board[r][c] = board[r + 1][c] = board[r + 1][c + 1] = board[r + 1][c + 2] = 0
@@ -18,8 +18,8 @@ def solution(board):
                 return True
 
     def isB(r, c):
-        if r + 2 <= n - 1 and c - 1 >= 0 and board[r][c] == board[r + 1][c] == board[r + 2][c] == board[r + 2][
-            c - 1]:
+        if r + 2 <= n - 1 and c - 1 >= 0 and \
+                board[r][c] == board[r + 1][c] == board[r + 2][c] == board[r + 2][c - 1]:
             if column_clean_until_row(r, c) and column_clean_until_row(r + 2, c - 1):
                 board[r][c] = board[r + 1][c] = board[r + 2][c] = board[r + 2][c - 1] = 0
                 return True
@@ -71,11 +71,11 @@ if __name__ == '__main__':
     board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 7, 0, 0, 0, 0, 0, 0, 0, 0],
-             [7, 7, 7, 0, 0, 0, 4, 0, 0, 0],
-             [0, 6, 0, 0, 0, 4, 4, 0, 0, 0],
-             [0, 6, 0, 0, 3, 0, 4, 0, 0, 0],
-             [6, 6, 0, 2, 3, 0, 0, 0, 5, 5],
+             [6, 7, 0, 0, 0, 0, 0, 0, 0, 0],
+             [6, 7, 7, 7, 0, 0, 4, 0, 0, 0],
+             [6, 6, 0, 0, 0, 4, 4, 0, 0, 0],
+             [0, 0, 0, 0, 3, 0, 4, 0, 0, 0],
+             [0, 0, 0, 2, 3, 0, 0, 0, 5, 5],
              [1, 2, 2, 2, 3, 3, 0, 0, 0, 5],
              [1, 1, 1, 0, 0, 0, 0, 0, 0, 5]
              ]
