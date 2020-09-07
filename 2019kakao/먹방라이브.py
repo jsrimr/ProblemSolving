c5140c1ce078c07fb0fa8c024431e3c791329d91
jsrimr <jsrimr@naver.com>
2020-09-07 23:52:16 +0900

    1. for 문 돌면서 자연스럽게 처리되는 것들이 있음. 굳이 다 잡으려 하지 않아도 됨 2. 기준을 하나로 하자. idx 와 time 2개로 생각하지 말고. 문제의 신비함을 다 없애라. 3.  변수 이름을 잘 짓자. 좀 길어도 된다. n_remain_food 4. t 가 아니라 t-jump 를 빼줘야..
    # n = len(sorted_f_times)
        # prev = 0 # 기준이 idx 와 time 2개가 아니라 하나로 만들자
        # cnt = 0
        # while True:
        #     idx = prev
        #     t, _ = sorted_f_times[idx]
        #     # while sorted_f_times[idx][0] == t:  # ->  굳이 이렇게 모아서 처리하려 하지 않아도 됨
    
        #     if t * (n-idx) > k: # -> 틀린이유. t 가 아니라 t-jump_time
        #         break

--- 2019kakao/먹방라이브.py
+++ 2019kakao/먹방라이브.py
@@ -1,30 +1,49 @@
 def solution(food_times, k):
-    food_times_with_idx = [(time, i) for i, time in enumerate(food_times)]
+    food_times_with_idx = [(time, i + 1) for i, time in enumerate(food_times)]
     sorted_f_times = sorted(food_times_with_idx)
 
-    n = len(sorted_f_times)
-    prev = 0
-    cnt = 0
-    while True:
-        idx = prev
-        t, _ = sorted_f_times[idx]
-        while sorted_f_times[idx][0] == t:
-            idx += 1
+    jump_time = 0
+    n_remain_food = len(sorted_f_times)
+    accum_time = 0
+    for t, _ in sorted_f_times:
+        accum_time += (t-jump_time) * n_remain_food
 
-        if t * (n-idx) > k:
+        if accum_time > k:
+            accum_time -= (t-jump_time) * n_remain_food
             break
-        else:
-            cnt += t * (n-idx)
-            prev = idx
 
-    candidate = sorted(sorted_f_times[prev+1:], key=lambda x: x[1])
+        jump_time = t
+        n_remain_food -= 1
 
-    return candidate[(k - cnt) % len(candidate)][1]
+    if n_remain_food <= 0:
+        return -1
+
+    candidate = sorted(sorted_f_times[-n_remain_food:], key=lambda x: x[1])
+    return candidate[(k - accum_time) % n_remain_food][1]
+
+    # n = len(sorted_f_times)
+    # prev = 0 # 기준이 idx 와 time 2개가 아니라 하나로 만들자
+    # cnt = 0
+    # while True:
+    #     idx = prev
+    #     t, _ = sorted_f_times[idx]
+    #     # while sorted_f_times[idx][0] == t:  # ->  굳이 이렇게 모아서 처리하려 하지 않아도 됨
+    #     #     idx += 1
+    #
+    #     if t * (n-idx) > k: # -> 틀린이유. t 가 아니라 t-jump_time
+    #         break
+    #     else:
+    #         cnt += t * (n-idx)
+    #         prev = idx
+
+    # candidate = sorted(sorted_f_times[prev + 1:], key=lambda x: x[1])
+    #
+    # return candidate[(k - cnt) % len(candidate)][1]
 
 
 if __name__ == '__main__':
     food_times = [3, 1, 6]
     k = 6
-    # food_times = [3, 1, 2]
-    # k = 5
+    food_times = [3, 1, 2]
+    k = 5
     print(solution(food_times, k))
