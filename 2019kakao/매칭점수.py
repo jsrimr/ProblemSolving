50a383b3ba81adc406939bec94135f8c3fee7a59
jsrimr <jsrimr@naver.com>
2020-09-08 16:46:20 +0900

    정규표현식 사용하는 법은 복습했지만 답은 틀리는구나.. 왜일까

---
+++ 2019kakao/매칭점수.py
@@ -0,0 +1,56 @@
+import re
+
+
+def solution(word, pages):
+    info = {}
+    # 페이지 이름 구하기 - head.meta
+    word = word.lower()
+    for i, page in enumerate(pages):
+        page = page.lower()
+        page_name = re.search(r'meta .* content="(?P<page_name>.*)"/>', page).group("page_name")
+
+        basic_score = re.findall("[a-z]+", page).count(word)
+        links = re.findall('<a href="(.*)">', page)
+
+        info[page_name] = {
+            # 기본 점수 구하기  - body
+            # 한 웹페이지의 기본점수는 해당 웹페이지의 텍스트 중, 검색어가 등장하는 횟수이다. (대소문자 무시)
+            "basic_score": basic_score,
+            "links": links,
+            "link_score_to_give": basic_score / len(links) if links else 0,
+            "idx": i,
+            "total_score": basic_score
+        }
+
+    # 링크점수 구하기 -> 매칭점수 구함
+    # 한 웹페이지의 외부 링크 수는 해당 웹페이지에서 다른 외부 페이지로 연결된 링크의 개수이다.
+    # 한 웹페이지의 링크점수는 해당 웹페이지로 링크가 걸린 다른 웹페이지의 기본점수 ÷ 외부 링크 수의 총합이다.
+    for page in info.values():
+        for link in page['links']:
+            if link in info:
+                referred_page = info[link]
+                referred_page['total_score'] += page['link_score_to_give']
+
+    max_total_score = 0
+    answer = -1
+    for page in info.values():
+
+        if page['total_score'] > max_total_score:
+            max_total_score = page['total_score']
+            answer = page['idx']
+
+    return answer
+
+
+if __name__ == '__main__':
+    word = "blind"
+    pages = [
+        "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://a.com\"/>\n</head>  \n<body>\nBlind Lorem Blind ipsum dolor Blind test sit amet, consectetur adipiscing elit. \n<a href=\"https://b.com\"> Link to b </a>\n</body>\n</html>",
+        "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://b.com\"/>\n</head>  \n<body>\nSuspendisse potenti. Vivamus venenatis tellus non turpis bibendum, \n<a href=\"https://a.com\"> Link to a </a>\nblind sed congue urna varius. Suspendisse feugiat nisl ligula, quis malesuada felis hendrerit ut.\n<a href=\"https://c.com\"> Link to c </a>\n</body>\n</html>",
+        "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://c.com\"/>\n</head>  \n<body>\nUt condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interdum nulla tempor nec. Phasellus rutrum enim at orci consectetu blind\n<a href=\"https://a.com\"> Link to a </a>\n</body>\n</html>"]
+    #
+    word = "Muzi"
+    pages = [
+        "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://careers.kakao.com/interview/list\"/>\n</head>  \n<body>\n<a href=\"https://programmers.co.kr/learn/courses/4673\"></a>#!MuziMuzi!)jayg07con&&\n\n</body>\n</html>",
+        "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://www.kakaocorp.com\"/>\n</head>  \n<body>\ncon%\tmuzI92apeach&2<a href=\"https://hashcode.co.kr/tos\"></a>\n\n\t^\n</body>\n</html>"]
+    print(solution(word, pages))
