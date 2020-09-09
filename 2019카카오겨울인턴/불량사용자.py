e09a3438ce1ff7f4eb5bb67f4fd0acb8e2a188e8
jsrimr <jsrimr@naver.com>
2020-09-09 22:30:31 +0900

    정규표현식으로 하면 abc123 에다가 ***** 적용해도 bc123 이런식으로 잡는 문제가 있어서 그냥 문자열처리방식으로 했는데, 그래도 틀리네

--- 2019카카오겨울인턴/불량사용자.py
+++ 2019카카오겨울인턴/불량사용자.py
@@ -1,14 +1,20 @@
 import itertools
-import re
-
 
 def solution(user_id, banned_id):
-    possible_combination = set()
 
     candidates_per_ban_id = []
     for ban_id in banned_id:
-        candidates_per_ban_id.append(re.findall(ban_id.replace("*", ".{1}"), "\n".join(user_id)))
+        tmp = []
+        for u_id in user_id:
+            if len(ban_id) != len(u_id):
+                continue
+            else:
+                if all([ban_id[i] == u_id[i] if ban_id[i] != "*" else True for i in range(len(ban_id))]):
+                    tmp.append(u_id)
+        candidates_per_ban_id.append(tmp)
+        # candidates_per_ban_id.append(re.findall(ban_id.replace("*", ".{1}"), "\n".join(user_id) + "\n"))
 
+    possible_combination = set()
     for combination in itertools.product(*candidates_per_ban_id):
         comb_set = set(combination)
         if len(comb_set) == len(banned_id):
@@ -20,7 +26,10 @@ def solution(user_id, banned_id):
 if __name__ == '__main__':
     user_id = ["frodo", "fradi", "crodo", "abc123", "frodoc"]
     banned_id = ["fr*d*", "abc1**"]
+    print(solution(user_id, banned_id))
+
+    user_id, banned_id = ["frodo", "fradi", "crodo", "abc123", "frodoc"], ["*rodo", "*rodo", "******"]
+    print(solution(user_id, banned_id))
 
     user_id, banned_id = ["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "*rodo", "******", "******"]
-    user_id, banned_id = ["frodo", "fradi", "crodo", "abc123", "frodoc"], ["*rodo", "*rodo", "******"]
     print(solution(user_id, banned_id))
