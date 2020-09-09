160c2bfaa07788797bb739241120b907d313075e
jsrimr <jsrimr@naver.com>
2020-09-09 23:56:50 +0900

    순열을 이미 다 만들어두고 대부분을 버리는 전략. 기존에는 조합을 이미 다 만들어두고 이 조합이 조건을 만족하는지 볼라했었음. 그런데, 그렇게 하면 가능한 조합인데도 u_id 와 ban_id 의 짝짓는 걸 잘해야하는 문제가 생겨서 구현이 안됨. 하지만 순열로 해두면 모든 경우를 다 고려하므로 가능한 조합인지 판단하기가 쉬움

--- 2019카카오겨울인턴/불량사용자.py
+++ 2019카카오겨울인턴/불량사용자.py
@@ -1,26 +1,52 @@
-import itertools
+# import itertools
+
+# def solution(user_id, banned_id):
+#
+#     candidates_per_ban_id = []
+#     for ban_id in banned_id:
+#         tmp = []
+#         for u_id in user_id:
+#             if len(ban_id) != len(u_id):
+#                 continue
+#             else:
+#                 if all([ban_id[i] == u_id[i] if ban_id[i] != "*" else True for i in range(len(ban_id))]):
+#                     tmp.append(u_id)
+#         candidates_per_ban_id.append(tmp)
+#         # candidates_per_ban_id.append(re.findall(ban_id.replace("*", ".{1}"), "\n".join(user_id) + "\n"))
+#
+#     possible_combination = set()
+#     for combination in itertools.product(*candidates_per_ban_id):
+#         comb_set = set(sorted(combination))
+#         if len(comb_set) == len(banned_id):
+#             possible_combination.add(tuple(comb_set))
+#
+#     return len(possible_combination)
+
+from itertools import permutations
 
 def solution(user_id, banned_id):
+    id_set = permutations(user_id, len(banned_id))
+    answer = set()
 
-    candidates_per_ban_id = []
-    for ban_id in banned_id:
-        tmp = []
-        for u_id in user_id:
+    def match(uid_set):
+        #set vs set
+        for i in range(len(uid_set)):
+            ban_id = banned_id[i]
+            u_id = uid_set[i]
+            # id vs id
             if len(ban_id) != len(u_id):
-                continue
+                return False
             else:
-                if all([ban_id[i] == u_id[i] if ban_id[i] != "*" else True for i in range(len(ban_id))]):
-                    tmp.append(u_id)
-        candidates_per_ban_id.append(tmp)
-        # candidates_per_ban_id.append(re.findall(ban_id.replace("*", ".{1}"), "\n".join(user_id) + "\n"))
+                if not all([ban_id[i] == u_id[i] if ban_id[i] != "*" else True for i in range(len(ban_id))]):
+                    return False
+        return True
 
-    possible_combination = set()
-    for combination in itertools.product(*candidates_per_ban_id):
-        comb_set = set(sorted(combination))
-        if len(comb_set) == len(banned_id):
-            possible_combination.add(tuple(comb_set))
+    for uid_set in id_set:
+        if match(uid_set):
+            answer.add(tuple(sorted(uid_set)))
 
-    return len(possible_combination)
+    return len(answer)
+
 
 
 if __name__ == '__main__':
