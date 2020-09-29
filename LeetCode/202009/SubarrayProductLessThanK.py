1f14b4a875a8404f36cc94de16335e075ded0681
jsrimr <jsrimr@naver.com>
2020-09-29 22:08:01 +0900

    투포인터. for 문을 이용해서 풀다 잘못됨

--- LeetCode/202009/SubarrayProductLessThanK.py
+++ LeetCode/202009/SubarrayProductLessThanK.py
@@ -3,21 +3,22 @@ from typing import List
 
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        # two pointer 로 : 언제 beg 를 움직이고 언제 end 를 움직일거나.
+        # P<k 면 end 를 키우고 P >= k 면 beg 를 키운다. 이 때,  beg 를 키울 때 subarray 개수를 계산해서 더해준다.
 
-        N = len(nums)
-        answer = 0
+        beg, end, N, P, answer = 0, 0, len(nums), 1, 0
 
-        def backtrack(i, k):  # 첫번째 원소는 포함한다고 했을 때
-            nonlocal answer
-            if k > 1:
-                answer += 1
-                # for j in range(i + 1, N):
-                # print(i)
-                if i + 1 < N:
-                    backtrack(i + 1, k / nums[i+1])
+        for beg in range(N):
+            end = max(beg, end)
+            while end < N:
+                if P * nums[end] < k:
+                    P *= nums[end]
+                    end += 1
+                else:
+                    break
 
-        for i in range(N):
-            backtrack(i, k / nums[i])
+            answer += (end - beg)
+            P /= nums[beg]
 
         return answer
 
@@ -25,5 +26,8 @@ class Solution:
 if __name__ == '__main__':
     s = Solution()
 
-    print(s.numSubarrayProductLessThanK(nums=[10, 5, 2, 6], k=100))
-    print(s.numSubarrayProductLessThanK([1, 1, 1], 2))
+    # print(s.numSubarrayProductLessThanK(nums=[10, 5, 2, 6], k=100))
+    # print(s.numSubarrayProductLessThanK([1, 1, 1], 2))
+    # print(s.numSubarrayProductLessThanK([1,2,3], 0))
+    # print(s.numSubarrayProductLessThanK([1,1,10,1,1], 5))
+    print(s.numSubarrayProductLessThanK([5,6,7,2], 5))
