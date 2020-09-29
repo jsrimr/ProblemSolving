7284cb7a49273580c20fc58967c12a9272a01c56
jsrimr <jsrimr@naver.com>
2020-09-29 15:48:26 +0900

    시간초과 나는 코드

---
+++ LeetCode/202009/SubarrayProductLessThanK.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+
+        N = len(nums)
+        answer = 0
+
+        def backtrack(i, k):  # 첫번째 원소는 포함한다고 했을 때
+            nonlocal answer
+            if k > 1:
+                answer += 1
+                for j in range(i + 1, N):
+                    backtrack(j, k / nums[i])
+
+        for i in range(N):
+            backtrack(i, k / nums[i])
+
+        return answer
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    print(s.numSubarrayProductLessThanK(nums=[10, 5, 2, 6], k=100))
