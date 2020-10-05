a7df89fdff1b04e3b7a8c9f7a2c487342d95ef68
jsrimr <jsrimr@naver.com>
2020-10-05 13:54:30 +0900

    먼저 나왔더라도 st 가 같은 경우 없어질 수 있는 가능성이 있었음. sort 를 ed 에 대해서 reverse 추가정렬해 해결

--- LeetCode/202010/RemoveCoveredInterval.py
+++ LeetCode/202010/RemoveCoveredInterval.py
@@ -1,20 +1,24 @@
 from typing import List
 
+
 class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort()
+        intervals.sort(key=lambda x: (x[0], -x[1]))
 
         removed = 0
         max_ed = 0
         for itv in intervals:
             st, ed = itv
-            if ed < max_ed:
+            if ed <= max_ed:
                 removed += 1
             max_ed = max(max_ed, ed)
 
         return len(intervals) - removed
 
+
 if __name__ == '__main__':
     s = Solution()
-    print(s.removeCoveredIntervals([[1,4],[3,6],[2,8]]))
-    print(s.removeCoveredIntervals(intervals = [[1,4],[2,3]]))
+    print(s.removeCoveredIntervals([[1, 4], [3, 6], [2, 8]]))
+    print(s.removeCoveredIntervals(intervals=[[1, 4], [2, 3]]))
+    print(s.removeCoveredIntervals([[3, 10], [4, 10], [5, 11]]))
+    print(s.removeCoveredIntervals([[1, 2], [1, 4], [3, 4]]))
