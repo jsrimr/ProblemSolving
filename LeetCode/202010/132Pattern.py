c616049886d951b3f46b5d017c284bd693f053b9
jsrimr <jsrimr@naver.com>
2020-10-23 22:43:23 +0900

    < 를 <= 로 수정

--- LeetCode/202010/132Pattern.py
+++ LeetCode/202010/132Pattern.py
@@ -10,7 +10,7 @@ class Solution:
 
         stack = []
         for i in range(n - 1, 0, -1):
-            while stack and stack[-1] < min_arr[i]:
+            while stack and stack[-1] <= min_arr[i]:
                 stack.pop()
 
             if stack and stack[-1] < nums[i]:
